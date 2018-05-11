f07a59cbaaa39ce8799eba4d592d604ae433f429
Tony Brown <tony@tonybrown.me>
2018-05-11 07:22:30 -0400

    make arbitrary change

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hello: 'there full stack dev!!!' });
+  res.send({ hello: 'there full stack dev 🚀' });
 });
 
 const PORT = process.env.PORT || 5000;
