3144244109a0649515238cfa12b93d72869a2207
Tony Brown <tony@tonybrown.me>
2018-05-12 08:24:38 -0400

    work on Google oauth

--- server/index.js
+++ server/index.js
@@ -1,9 +1,22 @@
-const express = require('express');
-const app = express();
+const express        = require('express')
+const passport       = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const keys = require('./config/keys')
 
-app.get('/', (req, res) => {
-  res.send({ hello: 'there full stack dev 🚀' });
-});
+const app = express()
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+passport.use(new GoogleStrategy(
+  {
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback',
+  }, 
+    (accessToken) => {
+      console.log(accessToken);
+    }
+  )
+);
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT)
